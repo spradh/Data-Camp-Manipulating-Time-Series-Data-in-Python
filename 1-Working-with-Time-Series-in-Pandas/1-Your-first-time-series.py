ef9e6f63fbcdbee4d9bdc671dfd8eb1538e1aeea
Sabin <spradh@users.noreply.github.com>
2023-12-10 08:58:36 -0600

    Update 1-Your-first-time-series.py

--- 1-Working-with-Time-Series-in-Pandas/1-Your-first-time-series.py
+++ 1-Working-with-Time-Series-in-Pandas/1-Your-first-time-series.py
@@ -1,1 +1,6 @@
-#TODO
+# Create the range of dates here
+seven_days = pd.date_range(start = '2017-1-1', periods = 7, freq = 'D')
+
+# Iterate over the dates and print the number and name of the weekday
+for day in seven_days:
+    print(day.dayofweek, day.day_name())
