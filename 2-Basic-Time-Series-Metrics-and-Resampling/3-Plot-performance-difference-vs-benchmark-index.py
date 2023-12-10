e622ecff7eeefbe3c87fc3823e6f54677da09f79
Sabin <spradh@users.noreply.github.com>
2023-12-10 15:31:41 -0600

    Update 3-Plot-performance-difference-vs-benchmark-index.py

--- 2-Basic-Time-Series-Metrics-and-Resampling/3-Plot-performance-difference-vs-benchmark-index.py
+++ 2-Basic-Time-Series-Metrics-and-Resampling/3-Plot-performance-difference-vs-benchmark-index.py
@@ -9,11 +9,11 @@ sp500 = pd.read_csv('sp500.csv', parse_dates = ['date'], index_col = 'date')
 
 # Concatenate stocks and index here
 data = pd.concat([stocks, sp500], axis = 1).dropna()
-data.info()
+print(data.info())
 
 # Normalize data
 normalized = data.div(data.iloc[0]).mul(100)
 
 # Subtract the normalized index from the normalized stock prices, and plot the result
-normalized.sub(normalized.SP500).plot()
+normalized[tickers].sub(normalized.SP500, axis = 0).plot()
 plt.tight_layout(); plt.show()
