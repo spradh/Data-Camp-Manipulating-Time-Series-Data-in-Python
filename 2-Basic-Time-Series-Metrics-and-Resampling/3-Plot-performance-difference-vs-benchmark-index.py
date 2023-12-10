ebe8475a502b8561357822d5c2816317b1cc7c58
Sabin <spradh@users.noreply.github.com>
2023-12-10 15:24:51 -0600

    Update 3-Plot-performance-difference-vs-benchmark-index.py

--- 2-Basic-Time-Series-Metrics-and-Resampling/3-Plot-performance-difference-vs-benchmark-index.py
+++ 2-Basic-Time-Series-Metrics-and-Resampling/3-Plot-performance-difference-vs-benchmark-index.py
@@ -1,1 +1,19 @@
+# Create tickers
+tickers = ['MSFT', 'AAPL']
 
+# Import stock data here
+stocks = pd.read_csv('msft_aapl.csv', parse_dates = ['date'], index_col = 'date')
+
+# Import index here
+sp500 = pd.read_csv('sp500.csv', parse_dates = ['date'], index_col = 'date')
+
+# Concatenate stocks and index here
+data = pd.concat([stocks, sp500], axis = 1).dropna()
+data.info()
+
+# Normalize data
+normalized = data.div(data.iloc[0]).mul(100)
+
+# Subtract the normalized index from the normalized stock prices, and plot the result
+normalized.sub(normalized.SP500).plot()
+plt.tight_layout(); plt.show()
