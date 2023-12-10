dc6ca79bed1051b0de7e56a2c62bec7156fe3d23
Sabin <spradh@users.noreply.github.com>
2023-12-10 15:02:13 -0600

    Update 1-Compare-the-performance-of-several-asset-classes.py

--- 2-Basic-Time-Series-Metrics-and-Resampling/1-Compare-the-performance-of-several-asset-classes.py
+++ 2-Basic-Time-Series-Metrics-and-Resampling/1-Compare-the-performance-of-several-asset-classes.py
@@ -1,1 +1,15 @@
-TODO:
+# Import data here
+prices = pd.read_csv("asset_classes.csv", index_col = 'DATE', parse_dates = ['DATE'])
+
+# Inspect prices here
+print(prices.info())
+
+# Select first prices
+first_prices = prices.iloc[0]
+
+# Create normalized
+normalized = prices.div(first_prices).mul(100)
+
+# Plot normalized
+normalized.plot()
+plt.tight_layout(); plt.show()
