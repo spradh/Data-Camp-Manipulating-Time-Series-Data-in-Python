d81d8fc46178770e1cc0e1d63c1580f2cd74b8cb
Sabin <spradh@users.noreply.github.com>
2023-12-12 17:00:38 -0600

    Update 1-Rolling-average-air-quality-since-2010-for-new-york-city.py

--- 3-Window-Functions-Rolling-and-Expanding-Metrics/1-Rolling-average-air-quality-since-2010-for-new-york-city.py
+++ 3-Window-Functions-Rolling-and-Expanding-Metrics/1-Rolling-average-air-quality-since-2010-for-new-york-city.py
@@ -1,1 +1,11 @@
-TODO:
+# Import and inspect ozone data here
+data = pd.read_csv('ozone.csv', parse_dates = ['date'], index_col = 'date')
+print(data.info())
+
+# Calculate 90d and 360d rolling mean for the last price
+data['90D'] = data['Ozone'].rolling(window = '90D').mean()
+data['360D'] = data['Ozone'].rolling(window = '360D').mean()
+
+# Plot data
+data['2010':].plot(title = 'New York City')
+plt.tight_layout(); plt.show()
