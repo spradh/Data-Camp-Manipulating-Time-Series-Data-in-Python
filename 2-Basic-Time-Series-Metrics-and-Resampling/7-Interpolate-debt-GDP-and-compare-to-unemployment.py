59826fadb25f5af32040035a06e861eabfc5bc0c
Sabin <spradh@users.noreply.github.com>
2023-12-11 20:52:32 -0600

    Create 7-Interpolate-debt-GDP-and-compare-to-unemployment.py

---
+++ 2-Basic-Time-Series-Metrics-and-Resampling/7-Interpolate-debt-GDP-and-compare-to-unemployment.py
@@ -0,0 +1,12 @@
+# Import & inspect data here
+data = pd.read_csv('debt_unemployment.csv', parse_dates = ['date'], index_col = 'date')
+print(data.info())
+
+# Interpolate and inspect here
+interpolated = data.interpolate()
+print(interpolated.info())
+
+
+# Plot interpolated data here
+interpolated.plot(secondary_y = "Unemployment")
+plt.tight_layout(); plt.show()
