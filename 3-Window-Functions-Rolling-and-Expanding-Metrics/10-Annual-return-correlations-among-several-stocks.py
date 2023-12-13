2be16c494fecf28a1a0234fac8241f2a7e8a6b26
Sabin <spradh@users.noreply.github.com>
2023-12-12 21:11:53 -0600

    Create 10-Annual-return-correlations-among-several-stocks.py

---
+++ 3-Window-Functions-Rolling-and-Expanding-Metrics/10-Annual-return-correlations-among-several-stocks.py
@@ -0,0 +1,17 @@
+# Inspect data here
+print(data.info())
+
+# Calculate year-end prices here
+annual_prices = data.resample('A').last()
+
+# Calculate annual returns here
+annual_returns = annual_prices.pct_change()
+
+# Calculate and print the correlation matrix here
+correlations = annual_returns.corr()
+print(correlations)
+
+# Visualize the correlations as heatmap here
+
+sns.heatmap(correlations, annot = True)
+plt.show()
