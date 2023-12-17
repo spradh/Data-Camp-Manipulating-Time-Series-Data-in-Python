e000e2ad834e77de579782eecf8dec03b4da3ee8
Sabin <spradh@users.noreply.github.com>
2023-12-16 23:21:31 -0600

    Create 3-Import-index-component-price-information.py

---
+++ 4-Putting-It-All-Together-Building-a-Value-Weighted-Index/3-Import-index-component-price-information.py
@@ -0,0 +1,14 @@
+# Print tickers
+print(tickers)
+
+# Import prices and inspect result
+stock_prices = pd.read_csv('stock_prices.csv', parse_dates = ['Date'], index_col = 'Date')
+print(stock_prices.info())
+
+# Calculate the returns
+price_return = stock_prices.iloc[-1].div(stock_prices.iloc[0]).sub(1).mul(100)
+
+# Plot horizontal bar chart of sorted price_return   
+price_return.sort_values().plot(kind = 'barh', title = 'Stock Price Returns')
+plt.tight_layout(); plt.show()
+
