e0eac7e1e3db6b91644442ce2d4cbd01bc86b903
Sabin <spradh@users.noreply.github.com>
2023-12-17 13:08:40 -0600

    Update and rename 8Compare-index-performance-against-benchmark-I.py to 8-Compare-index-performance-against-benchmark-I.py

--- 4-Putting-It-All-Together-Building-a-Value-Weighted-Index/8-Compare-index-performance-against-benchmark-I.py
+++ 4-Putting-It-All-Together-Building-a-Value-Weighted-Index/8-Compare-index-performance-against-benchmark-I.py
@@ -1,15 +1,12 @@
 # Convert index series to dataframe here
-data = index.to_frame(name = 'Index')
-print(data)
-
+data = index.to_frame('Index')
 
 # Normalize djia series and add as new column to data
-djia_norm = (djia/djia[0]) * 100
+djia = djia.div(djia.iloc[0]).mul(100)
 data['DJIA'] = djia
-print(djia_norm)
 
 # Show total return for both index and djia
-print(data.iloc[-1].div(data.iloc[1]).sub(-1).mul(100))
+print(data.iloc[-1].div(data.iloc[0]).sub(1).mul(100))
 
 # Plot both series
 data.plot()
